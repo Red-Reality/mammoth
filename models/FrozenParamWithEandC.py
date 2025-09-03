f78da2412960b0f49046edcb2a06970136779295
Red-Reality <2153678@tongji.edu.cn>
2025-09-03 14:54:55 +0800

    完成debug，效果不如er

--- models/FrozenParamWithEandC.py
+++ models/FrozenParamWithEandC.py
@@ -13,21 +13,19 @@ from utils.buffer import Buffer
 
 class FrozenParamWithEandC(ContinualModel):
     """Continual learning via freezing parameters with E and C."""
-    NAME = 'frozen_param_with_e_and_c'
+    NAME = 'FrozenParamWithEandC'
     COMPATIBILITY = ['class-il', 'domain-il', 'task-il']
 
     @staticmethod
     def get_parser(parser: ArgumentParser) -> ArgumentParser:
-        parser.add_argument('--freeze_mask_path',type=str, required=True,
-                            help='Path to the freeze mask file')
         parser.add_argument('--freeze_rate',type=float, default=0.1,
                             help='Rate of parameters to freeze')
         parser.add_argument('--replay_train_epochs', type=int, default=5,
                             help='Number of epochs to train on replay data')
         
-        parser.add_argument('--alpha',dtype = float,default=0.5)
-        parser.add_argument('--beta',dtype = float,default= 2)
-        parser.add_argument('--num_of_observe_before_train_in_replaybuffer',dtype=int,default=5
+        parser.add_argument('--alpha',type = float,default=0.5)
+        parser.add_argument('--beta',type = float,default= 2)
+        parser.add_argument('--num_of_observe_before_train_in_replaybuffer',type=int,default=5,
                             help='Number of observe to train on current data before train one observe in replay data')
         # 添加回放参数
         add_rehearsal_args(parser)
@@ -48,6 +46,7 @@ class FrozenParamWithEandC(ContinualModel):
                 name:torch.ones_like(param,dtype=torch.float32,device=self.device)
                 for name,param in self.net.named_parameters()
             }
+        return super().begin_task(dataset)
     def end_task(self, dataset):
         '''
         任务结束后：
@@ -67,27 +66,6 @@ class FrozenParamWithEandC(ContinualModel):
         self.last_param =copy.deepcopy(self.net.state_dict())
 
 
-
-    def save_freeze_mask(self, path):
-        """保存冻结掩码"""
-        if self.freeze_masks is not None:
-            if not os.path.exists(os.path.dirname(path)):
-                os.makedirs(os.path.dirname(path))
-            torch.save(self.freeze_masks, path)
-        else:
-            raise ValueError("Freeze masks are not set. Please run the training process first.")
-
-    def load_freeze_mask(self, path):
-        """加载冻结掩码"""
-        if os.path.exists(path):
-            self.freeze_masks = torch.load(path, map_location=self.device)
-            
-        else:
-            self.freeze_masks={
-                name:torch.ones_like(param,dtype=torch.float32,device=self.device)
-                for name,param in self.net.named_parameters()
-            }
-        return self.freeze_masks
     def froze_param(self):
         """
         将变化最大的前百分之k的参数对应的freeze_mask设为0
@@ -97,9 +75,9 @@ class FrozenParamWithEandC(ContinualModel):
         with torch.no_grad():
             delta_param = copy.deepcopy(self.net.state_dict())
             if self.last_param is not None:
-                for name,param in self.net.state_dict():
+                for name,param in self.net.state_dict().items():
                     delta_param[name] = torch.abs(param-self.last_param[name])
-            param_vector = torch.cat([param.view(-1)for param in delta_param])
+            param_vector =torch.cat([param.view(-1) for _,param in delta_param.items()])
 
             # 相对比率正则化
             param_vector = param_vector / (torch.mean(param_vector) + 1e-10)
@@ -120,7 +98,7 @@ class FrozenParamWithEandC(ContinualModel):
                 num_param = param.numel()
                 self.freeze_masks[name] = mask_vector[pointer:pointer+num_param].view_as(param).to(self.device)
                 pointer += num_param
-    def observe(self, inputs: torch.Tensor, labels: torch.Tensor, not_aug_inputs: torch.Tensor, epoch: int = None) -> float:
+    def observe(self, inputs, labels, not_aug_inputs, epoch=None) -> float:
         '''
         训练步骤
         1. 使用当前任务的数据进行训练
@@ -134,7 +112,7 @@ class FrozenParamWithEandC(ContinualModel):
         self.opt.step()
         
         # 使用回放数据进行微调
-        if self.epoch_iteration % self.args.num_of_observe_before_train_in_replaybuffer == 0:
+        if self.epoch_iteration!=0 and self.epoch_iteration % self.args.num_of_observe_before_train_in_replaybuffer == 0:
             if not self.buffer.is_empty():
                 buf_inputs, buf_labels = self.buffer.get_data(self.args.minibatch_size, transform=self.transform, device=self.device)
                 self.net.zero_grad()
@@ -146,7 +124,7 @@ class FrozenParamWithEandC(ContinualModel):
                     raise ValueError("freeze_masks is None")
                 self.froze_param()
                 # log:检查还有多少可被训练的参数
-                froze_vector = torch.cat([mask.view(-1)for mask in self.freeze_masks])
+                froze_vector = torch.cat([mask.view(-1)for _,mask in self.freeze_masks.items()])
                 num_trainable = torch.sum(froze_vector).item()
                 print(f"Number of trainable parameters after freezing: {num_trainable}/{froze_vector.numel()}")
                 if self.freeze_masks is not None:
